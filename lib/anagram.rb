03ab69c6c8bf657adef28fad2964e1a4d4676fda
Hayden Betts <haydenbetts@gmail.com>
2018-06-27 23:13:17 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,12 @@
 class Anagram
+  attr_accessor :word
 
   def initialize(word)
     @word = word
   end
 
-  def word
-    @word
-  end
-  
   def match
     # takes an array of possible anagrams for @word
-    # returns the 
+    # returns the agram if it exists
   end
 end
