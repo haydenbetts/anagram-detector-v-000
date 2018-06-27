2e48ea5b8aa15ec6de1e4b664e4d915cc9c55107
Hayden Betts <haydenbetts@gmail.com>
2018-06-27 23:16:27 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,9 @@ class Anagram
 
   def match(anagram_array)
 
-    word_in_alphabetical_order = self.word.split('').sort.join
+    word_sorted = self.word.split('').sort.join
     anagram_array.each do |possible_anagram|
+      possible_anagram.split('').sort.join
 
     # takes an array of possible anagrams for @word
     # returns the agram if it exists
